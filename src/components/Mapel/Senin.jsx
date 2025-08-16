5f99c472976dba5b4f2465f2a30639c0a7e5e7d2
AliefKaka <aliefkaka123@gmail.com>
2025-08-16 20:39:49 +0700

    Create Senin.jsx

---
+++ src/components/Mapel/Senin.jsx
@@ -0,0 +1,52 @@
+const Senin = () => {
+    return (
+        <>
+        	<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Upacara</div>
+					<div className="flex justify-center items-center text-sm">06.45-07.30</div>
+				</div>
+          <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+					<div className="w-[50%] text-base  font-medium">Kimia</div>
+					<div className="flex justify-center items-center text-sm">07.30-08.15</div>
+				</div>
+          <div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+					<div className="w-[50%] text-base  font-medium">MTK-L</div>
+					<div className="flex justify-center items-center text-sm">08.00-09.45</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">09.45-10.00</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base  font-medium">Biologi</div>
+					<div className="flex justify-center items-center text-sm">10.00-10.45</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1100">
+					<div className="w-[50%] text-base  font-medium">BHS. Indonesia</div>
+					<div className="flex justify-center items-center text-sm">10.45-11.30</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="1200">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">11.30-12.15</div>
+				</div>
+          <div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1300">
+					<div className="w-[50%] text-base  font-medium">BHS. Indonesia</div>
+					<div className="flex justify-center items-center text-sm">12.15-13.00</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1400">
+					<div className="w-[50%] text-base font-medium">PAG Islam</div>
+					<div className="flex justify-center items-center text-sm">13.00-13.45</div>
+				</div>
+          <div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1500">
+					<div className="w-[50%] text-base  font-medium">Seni Budaya</div>
+					<div className="flex justify-center items-center text-sm">13.45-15.15</div>
+				</div>
+          <div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Ishoma</div>
+					<div className="flex justify-center items-center text-sm">14.15-15.30</div>
+				</div>
+        </>
+    )
+} 
+
+export default Senin;

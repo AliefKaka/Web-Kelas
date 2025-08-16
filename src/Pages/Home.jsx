88bac739c8e4b3b8386de2e47f684f4f9f3c185b
AliefKaka <aliefkaka123@gmail.com>
2025-08-16 22:23:38 +0700

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,4 @@
 import BoxClassIg from "../components/BoxClassIg"
-import BoxOldWeb from "../components/BoxOldWeb"
 import BoxTextAnonim from "../components/BoxTextAnonim"
 import Navbar from "../components/Navbar"
 
